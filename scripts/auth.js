49010835d0e1ea162913357bac51189a4fa0b450
mrschljakhta-max <mr.schljakhta@gmail.com>
2026-04-20 14:56:25 +0300

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -798,7 +798,7 @@ function bindEvents() {
 
   setupOtpGroup('mfaEnrollOtp', 'mfaEnrollCodeInput');
   setupOtpGroup('mfaVerifyOtp', 'mfaVerifyCodeInput');
-}
+} 
 
 async function initAuthPage() {
   bindEvents();
